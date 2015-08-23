e0d5e9004d7e2a0f0285ae05f1ae810bfddefdd6
Rbcum <956175818@qq.com>
2015-08-23 10:26:44 +0800

    fix setClipPanel doc typo

--- library/src/com/sothree/slidinguppanel/SlidingUpPanelLayout.java
+++ library/src/com/sothree/slidinguppanel/SlidingUpPanelLayout.java
@@ -623,7 +623,7 @@ public class SlidingUpPanelLayout extends ViewGroup {
     /**
      * Sets whether or not the main content is clipped to the top of the panel
      *
-     * @param overlayed
+     * @param clip
      */
     public void setClipPanel(boolean clip) {
         mClipPanel = clip;
